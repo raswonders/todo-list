8e6d9eea72bd7f1caee54628827c67c269a2560d
Rastislav Hepner <rastislav.hepner@gmail.com>
2021-05-13 21:09:32 +0200

    add task implemented

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
 (function() {
-    var textBar = document.querySelector('.text-bar')
-    var taskForm = document.querySelector('.task-form')
-    var objList = []
+    const textBarDOM = document.querySelector('.text-bar')
+    const taskFormDOM = document.querySelector('.task-form')
+    const taskListDOM = document.querySelector('.task-list')
+    var taskList = []
 
-    function task(msg) {
+    function Task(msg) {
         this.done = false;
         this.msg = msg;
     }
 
+    function clearMsg() {
+        textBarDOM.value = ""
+    }
 
-    taskForm.addEventListener('submit', function addTask(e) {
+    function clearTasks() {
+        taskListDOM.innerHTML = ""
+    }
+
+    function displayTasks(tasks) {
+        for (let t of tasks) {
+            const el = document.createElement('li')
+            const icons = `<div><i class="far fa-check-circle"></i><i class="far fa-edit"></i><i class="far fa-times-circle"></i></div>`
+            el.innerHTML = `<div>${t.msg}</div>${icons}`
+            el.classList.add("task")
+            if (t.done) {
+                el.classList.add("done")
+            }
+            taskListDOM.appendChild(el)
+        }
+    }
+
+    taskFormDOM.addEventListener('submit', function addTask(e) {
         e.preventDefault()
 
-        objList.push(new task(textBar.value))
-        textBar.value = ""
+        clearMsg()
+        clearTasks()
+        taskList.push(new Task(textBarDOM.value))
+        displayTasks(taskList)
     })
 })();
